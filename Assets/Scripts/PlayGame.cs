906636b9b768ac91f3c66f521d34e881f1be7446
Zarrickisdea <mohammedraz45@yahoo.com>
2023-06-14 21:05:04 +0530

    Play and Quit Buttons

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayGame : MonoBehaviour
+{
+    public Button playButton;
+    public Button quitButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(EnterGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void EnterGame()
+    {
+        if (playButton != null) 
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    void QuitGame()
+    { 
+        if (quitButton != null) 
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+    }
+}
